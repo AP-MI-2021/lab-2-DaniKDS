e1323f04e4d3aea3d831ee99378f753d08356d43
DaniKDS <91533585+DaniKDS@users.noreply.github.com>
2021-10-06 14:50:41 +0300

    am facut mofificarile cerute

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import math
 #Problema 6 // verificare palindrom
-def isPalindrome(s):
+def is_palindrome(n) -> bool:
+
+    '''
+    Verificare palindrom
+    :param n: introducem numarul pe care il verificam daca este palindrom sau nu
+    :return: o sa se returneze True daca este palindrom,False in caz contrar
+    :i,j:capetele numarului ,inceput,respectiv sfarsit
+    '''
+
     i = 0
-    j = len(s) - 1
+    j = len(n) - 1
     while i <= j :
-        if s[i] != s[j]:
+        if n[i] != n[j]:
             return False
         i = i + 1
         j = j - 1
@@ -12,18 +20,23 @@ def isPalindrome(s):
 
 def test_is_palindrome():
 
-    assert isPalindrome('ana')
-    assert isPalindrome('mar') == False
-    assert isPalindrome('mouse') == False
-    assert isPalindrome('maam')
-    assert isPalindrome('bal') == False
+    assert is_palindrome('121') ==True
+    assert is_palindrome('123') == False
+    assert is_palindrome('555') == True
+    assert is_palindrome('2233') == False
+    assert is_palindrome('226622') == True
 
 test_is_palindrome()
 
 #Problema 8 //transforma numarul din baza 10 in baza 2
 def get_base_2(n:str):
-    n = int(n)
+    '''
+    Transforma numarul din baza 10 in baza 2
+    :param n:numarul pe care vrem sa-l convertim din baza 10 in baza 2
+    output: ans- variabila in care apare numarul convertit
+    '''
     ans = ""
+    n = int(n)
     while n > 0 :
         if n % 2 == 0:
             ans = '0' +ans
@@ -44,22 +57,24 @@ test_get_base_2()
 
 #Problema 12 //afiseaza patratele perfecte dintr-un interval inchis.
 
-def is_perfect(n):
-    if int(math.sqrt(n)) == math.sqrt(n):
-        return True
-    return False
-
-def get_perfect_squares(start: int, end: int):
-    for i in range( start , end +1 ):
-        if is_perfect(i):
-            print(f'Numarul {i} este patrat perfect')
-        else:
-            print(f'Numarul {i} nu este patrat perfect')
+def get_perfect_squares(a, b):
+    '''
+    Cautam toate patratele perfecte din intervalul [a,b]
+    - a, b, intregi, capetele intervalului
+    -lst, lista patratlor perfecte
+    '''
+    radacina = int(math.sqrt(a))
+    lst = []
+    if radacina * radacina < a:
+        radacina = radacina + 1
+    while radacina <= math.sqrt(b):
+        lst.append(radacina * radacina)
+        radacina = radacina + 1
+    return lst
 
 def test_get_perfect_squares():
-    assert is_perfect(4) == True
-    assert is_perfect(16) == True
-    assert is_perfect(7) == False
+    assert get_perfect_squares(1, 9) == [1, 4, 9]
+    assert get_perfect_squares(3, 25) == [4, 9, 16, 25]
 
 def main():
 
@@ -70,12 +85,12 @@ def main():
 
         optiune = input ("Alegeti problema:")
         if optiune == '1':
-            cuvant = input('Dati cuvantul care se cere sa fie verificat: ')
-            ans = isPalindrome(cuvant)
+            numar = input('Dati numarul care se cere sa fie verificat: ')
+            ans = is_palindrome(numar)
             if ans:
-                print ("Cuvantul este un palindrom.")
+                print ("Numarul este un palindrom.")
             else:
-                print("Cuvantul nu este un palindrom.")
+                print("Numarul nu este un palindrom.")
 
         elif optiune == '2':
             x = input('Convertesc numarul: ')
@@ -83,14 +98,15 @@ def main():
             print(get_base_2(x))
 
         elif optiune == '3':
-            start = input('Stanga :')
-            end = input('Dreapta :')
-            get_perfect_squares(int(start) , int(end))
+            a = int(input("Dati inceput de interval: "))
+            b = int(input("Dati sfarsit de interval: "))
+            test_get_perfect_squares()
+            print(get_perfect_squares(a, b))
 
         elif optiune == 'x':
             break
         else:
             print('Optiune invalida!')
+
 if __name__ == '__main__':
     main()
-main()
