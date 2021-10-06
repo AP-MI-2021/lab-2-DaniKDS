90aa7579c8765d5cd1faeeef37ec4322b9d7e5cf
DaniKDS <91533585+DaniKDS@users.noreply.github.com>
2021-10-06 12:31:06 +0300

    am pus si a 3-a problema

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 #Problema 6 // verificare palindrom
 def isPalindrome(s):
     i = 0
@@ -41,10 +42,31 @@ def test_get_base_2():
 
 test_get_base_2()
 
+#Problema 12 //afiseaza patratele perfecte dintr-un interval inchis.
+
+def is_perfect(n):
+    if int(math.sqrt(n)) == math.sqrt(n):
+        return True
+    return False
+
+def get_perfect_squares(start: int, end: int):
+    for i in range( start , end +1 ):
+        if is_perfect(i):
+            print(f'Numarul {i} este patrat perfect')
+        else:
+            print(f'Numarul {i} nu este patrat perfect')
+
+def test_get_perfect_squares():
+    assert is_perfect(4) == True
+    assert is_perfect(16) == True
+    assert is_perfect(7) == False
+
 def main():
+
     while True:
         print('1.Verificam daca numarul dat este sau nu palindrom.')
         print('2.Fac conversie din baza 10 in baza 2.')
+        print('3.Afisez toate patratele perfecte dintr-un interval inchis')
 
         optiune = input ("Alegeti problema:")
         if optiune == '1':
@@ -60,9 +82,15 @@ def main():
             get_base_2(x)
             print(get_base_2(x))
 
+        elif optiune == '3':
+            start = input('Stanga :')
+            end = input('Dreapta :')
+            get_perfect_squares(int(start) , int(end))
+
         elif optiune == 'x':
             break
         else:
             print('Optiune invalida!')
-
+if __name__ == '__main__':
+    main()
 main()
